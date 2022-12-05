7681daf3dff0abb5db037417dd6e361cee1fec8b
Amir <fdhjdzcb@gmail.com>
2022-12-05 18:59:51 +0300

    Ограничения по полям

--- src/main/java/com/AMIR/SRM/SrmApplication.java
+++ src/main/java/com/AMIR/SRM/SrmApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SrmApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SrmApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SrmApplication.class, args);
 
-	}
+    }
 }
