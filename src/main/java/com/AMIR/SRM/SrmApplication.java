070f6327afcbe3cfb9fa74cf0240476eae566e93
Amir <fdhjdzcb@gmail.com>
2022-12-06 00:59:43 +0300

    Уехали заголовки

--- src/main/java/com/AMIR/SRM/SrmApplication.java
+++ src/main/java/com/AMIR/SRM/SrmApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SrmApplication {
     public static void main(String[] args) {
         SpringApplication.run(SrmApplication.class, args);
-
     }
 }
